6681ac1d64f2cf654bb54769fa5ddd82b86e0a8e
jungwon <mac20010@naver.com>
2023-08-11 18:07:53 +0900

    FEAT : Sorted Set으로 리더보드 구현

---
+++ RedisPractice/src/main/java/com/example/pratice/leaderboard/service/RankingService.java
@@ -0,0 +1,40 @@
+package com.example.pratice.leaderboard.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RankingService {
+
+    private static final String LEADERBOARD_KEY ="leaderBoard";
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    public boolean setUserScore(String userId, int score){
+        ZSetOperations zSetOps =redisTemplate.opsForZSet();
+        zSetOps.add(LEADERBOARD_KEY,userId,score);
+
+        return true;
+    }
+
+    public Long getUserRanking(String userId){
+        ZSetOperations zSetOps =redisTemplate.opsForZSet();
+        Long rank = zSetOps.reverseRank(LEADERBOARD_KEY, userId);
+
+        return rank;
+    }
+
+    public List<String> getTopRank(int limit){
+        ZSetOperations zSetOps = redisTemplate.opsForZSet();
+        Set<String> rangeSet = zSetOps.reverseRange(LEADERBOARD_KEY, 0, limit - 1);
+
+        return new ArrayList<>(rangeSet);
+    }
+
+}
